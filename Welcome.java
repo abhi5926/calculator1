0e30974bc2db4626ca855d0cdb254c766b8c57db
Abhishek Sen <168709700+abhi5926@users.noreply.github.com>
2025-10-03 15:56:52 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,22 +1,6 @@
-import java.util.Scanner;
 public class Welcome{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-System.out.println("Welcome to the calculator");
-int a;
-System.out.println("Enter first number");
-a=sc.nextInt();
-int b;
-System.out.println("Enter Second number");
-b=sc.nextInt();
-int ch;
-System.out.println("Enter your choice: 1. Addition /n 2.Substraction /n 3. Multiplication /n 4.Division");
-ch=sc.nextInt();
-switch(ch){
+public static void main(String[] args){
 
-}
-
-
-
+  System.out.println("Heelo I am Abhishek from Madhya Pradesh");
 }
 }
